1b5b7906c9d1c6ca830d1d4a1c0d7616c6a20876
PapsBurr <npons3932@gmail.com>
2025-12-24 12:28:00 -0700

    chore: update gitignore

--- README.md
+++
@@ -1,3 +0,0 @@
-# ecommerce-fullstack
-
-This web app is a sample ecommerce website. It hosts an Angular front-end (TypeScript) and a Springboot back-end (Java).
--- ecommerce-fullstack/spring-boot-ecommerce/src/main/java/com/practiceproject/ecommerce/dao/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practiceproject.ecommerce.dao;
-
-import com.practiceproject.ecommerce.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
